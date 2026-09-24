da5d79cca31d2b41999c3abd4d04908203c5f4e7
Iwadiasz <152991206+Iwadiasz@users.noreply.github.com>
2026-09-24 20:34:32 +0200

    Czesc 5

--- zadania/mb05/vite-project/src/components/PhotoCard.jsx
+++ zadania/mb05/vite-project/src/components/PhotoCard.jsx
@@ -1,6 +1,6 @@
 const NAZWA_KATEGORII = { gory: 'Góry', morze: 'Morze', miasto: 'Miasto' }
 const KOLOR_KATEGORII = { gory: 'success', morze: 'primary', miasto: 'dark' }
-function PhotoCard({ title, description, category, image, alt }) {
+function PhotoCard({ id, title, description, category, image, alt }) {
     return (
         <div className="card h-100 shadow-sm">
             <img src={image} className="card-img-top" alt={alt} />
@@ -12,7 +12,9 @@ function PhotoCard({ title, description, category, image, alt }) {
                     </span>
                 </p>
                 <p className="card-text text-body-secondary"> {description} </p>
-
+                <button type="button" className="btn btn-outline-primary mt-auto" data-bs-toggle="modal" data-bs-target={`#zdjecie${id}`}>
+                    Powiększ
+                </button>
             </div>
         </div>
     )
